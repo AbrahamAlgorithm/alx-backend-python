323bbfe6e3b5ce3fba1925aa208d6ae1b7cd4926
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-06-10 22:21:03 +0100

    The basics of async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,8 +4,7 @@ import random
 '''asynchronous coroutine that takes in an integer argument'''
 
 
-async def wait_random(max_delay: int = 10) => float:
+async def wait_random(max_delay: int = 10) -> float:
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
