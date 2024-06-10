507e5fc67ff62b3703d74fa2a2a5c05e3ff564e4
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-06-10 23:22:26 +0100

    Measure the runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+ a measure_time function with integers n and max_delay as arguments that measures the total execution time for wait_n(n, max_delay), and returns total_time / n
+'''
+
+
+import asyncio
+import time
+from typing import List
+
+
+wait_n = __import__('2-measure_runtime.py').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    '''An async functions which measures time'''
+    start_time = time.perf_counter()
+    await wait_n(n, max_delay)
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    return total_time / n
