44ab5c224b6d2a02fda955cd4e22a6bb24a8747a
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-06-10 23:52:26 +0100

    feat: Tasks

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,6 +8,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> int:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     '''An async function'''
     return asyncio.create_task(wait_random(max_delay))
