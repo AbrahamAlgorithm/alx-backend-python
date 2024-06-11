bc685ed088026e628cde4bb72c06be8fbabef6af
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-06-11 23:54:02 +0100

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''A coroutine called async_generator that takes no arguments.'''
+
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    '''The coroutine will loop 10 times'''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
