118800f24081712ce10444da98c7419a64de7d9c
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-06-12 00:34:47 +0100

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     '''The coroutine will loop 10 times'''
     for _ in range(10):
         await asyncio.sleep(1)
