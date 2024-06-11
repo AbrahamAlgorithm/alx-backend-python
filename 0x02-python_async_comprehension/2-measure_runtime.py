ba1c3cbede2c6ba6932e19b128bb5324afdcbc09
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-06-12 00:28:34 +0100

    Run time for four parallel comprehensions

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''A coroutine that will execute async_comprehension four times in parallel'''
+
+
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    '''The start time'''
+    start_time = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+    end_time = time.perf_counter()
+    return end_time - start_time
